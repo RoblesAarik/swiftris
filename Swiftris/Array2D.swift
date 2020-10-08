abcf30d5f5de9c88c70c38b7957901ea140b6d0a
aarik <roblesaarik@gmail.com>
2020-10-08 14:04:39 -0700

    created table

---
+++ Swiftris/Array2D.swift
@@ -0,0 +1,31 @@
+//
+//  Array2D.swift
+//  Swiftris
+//
+//  Created by Aarik Robles on 10/8/20.
+//
+
+// #1
+ class Array2D<T> {
+     let columns: Int
+     let rows: Int
+// #2
+     var array: Array<T?>
+
+     init(columns: Int, rows: Int) {
+         self.columns = columns
+         self.rows = rows
+// #3
+         array = Array<T?>(count:rows * columns, repeatedValue: nil)
+     }
+
+// #4
+     subscript(column: Int, row: Int) -> T? {
+         get {
+             return array[(row * columns) + column]
+         }
+         set(newValue) {
+             array[(row * columns) + column] = newValue
+         }
+     }
+ }
