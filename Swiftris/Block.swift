d3fe8e6dd912353b15893a6409f7adc160016307
aarik <roblesaarik@gmail.com>
2020-10-12 13:36:37 -0700

    finished blocks

--- Swiftris/Block.swift
+++ Swiftris/Block.swift
@@ -42,10 +42,49 @@ enum BlockColor: Int, CustomStringConvertible {
         return self.spriteName
     }
     
+// #6
     static func randon() -> BlockColor {
         return BlockColor(rawValue: Int(arc4random_uniform(NumberofColors)))!
     }
     
+//  #7
+    class Block: Hashable, CustomStringConvertible {
+        
+//   #8
+//        Constants
+        let color: BlockColor
+        
+//   #9
+//        Properties
+        var column: Int
+        var row: Int
+        var sprite: SKSpriteNode?
+        
+//  #10
+        var spriteName: String {
+            return color.spriteName
+        }
+        
+// #11
+        var hashValue: Int {
+            return self.column ^ self.row
+        }
+        
+// #12
+        var description: String {
+            return "\(color): [\(column), \(row)]"
+        }
+        
+
+        init(column:Int, row:Int, Color:BlockColor) {
+            self.column = column
+            self.row = row
+            self.color = color
+        }
+    }
     
+    func ==(lhs: Block, rhs: Block) -> Bool {
+        return lhs.column == rhs.column && lhs.row == rhs.row && lhs.color.rawValue == rhs.color.rawValue
+    }
     
 }
